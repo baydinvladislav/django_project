46ac3195b1a625343bb4ac41f67b4d02a6d2f6e4
Vladislav Baydin <baydinvladislav@yandex.ru>
2020-09-16 17:11:16 +0300

    add generation page

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailsView.as_view(), name='news-detail')
 ]
+
--- news/views.py
+++ news/views.py
@@ -4,7 +4,6 @@ from .forms import ArticlesForm
 from django.views.generic import DetailView
 
 
-
 def news_home(request):
     news = Articles.objects.order_by('date')
     return render(request, 'news/news_home.html', {'news': news})
@@ -12,7 +11,7 @@ def news_home(request):
 class NewsDetailsView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
-    context_object_name = 'Article'
+    context_object_name = 'article'
 
 def create(request):
     error = ''
