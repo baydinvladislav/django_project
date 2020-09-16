14b937aaa2b8c9c33b6432a6a425d13d9029f1d8
Vladislav Baydin <baydinvladislav@yandex.ru>
2020-09-16 16:32:42 +0300

    views.py test

--- news/views.py
+++ news/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
+
+
 
 def news_home(request):
-    news = Articles.objects.order_by('-date')
+    news = Articles.objects.order_by('date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsDetailsView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'Article'
 
 def create(request):
     error = ''
